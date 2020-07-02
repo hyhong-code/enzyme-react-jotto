9598f6e94aaa3a9e506e82fe5bb8f35242d8b77f
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-07-02 12:15:19 -0700

    Write test for when word has been guessed

--- src/Input.test.js
+++ src/Input.test.js
@@ -47,9 +47,31 @@ describe("render", () => {
   });
 
   describe("word has been guessed", () => {
-    test(`renders without error`, () => {});
-    test(`does not render the input box`, () => {});
-    test(`does not render the submit button`, () => {});
+    let wrapper;
+
+    beforeEach(() => {
+      const initialState = { success: true };
+      wrapper = setup(initialState);
+    });
+
+    afterEach(() => {
+      wrapper.unmount();
+    });
+
+    test(`renders without error`, () => {
+      const component = findByTestAttr(wrapper, "component-input");
+      expect(component.length).toBe(1);
+    });
+
+    test(`does not render the input box`, () => {
+      const inputBox = findByTestAttr(wrapper, "input-box");
+      expect(inputBox.length).toBe(0);
+    });
+
+    test(`does not render the submit button`, () => {
+      const submitButton = findByTestAttr(wrapper, "submit-button");
+      expect(submitButton.length).toBe(0);
+    });
   });
 });
 
