d4609500922cd1e852015595892ecf3ad9f6e139
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-07-02 11:15:46 -0700

    Write tests for successReducer

--- src/reducers/successReducer.js
+++ src/reducers/successReducer.js
@@ -1,11 +1,19 @@
+import { actionTypes } from "../actions/index";
+
 /**
  * @function successReducer
  * @param {object} state - Array of guessed words
  * @param {object} action - Action to be reduced
  * @return {boolean} - new success states
  */
-const successReducer = (state, action) => {
-  return null;
+const successReducer = (state = false, action) => {
+  // const { type, payload } = action;
+  switch (action.type) {
+    case actionTypes.CORRECT_GUESS:
+      return true;
+    default:
+      return state;
+  }
 };
 
 export default successReducer;
---
+++ src/reducers/successReducer.test.js
@@ -0,0 +1,15 @@
+import { actionTypes } from "../actions/index";
+import successReducer from "./successReducer";
+
+test(`returns defalt initial state when no action is passed`, () => {
+  // Must pass in an object for action, otherwise cannot read property type of undefined
+  const newState = successReducer(undefined, {});
+  expect(newState).toBe(false);
+});
+
+test(`returns state of true upon receiving an action of type "CORRECT_GUESS"`, () => {
+  const newState = successReducer(undefined, {
+    type: actionTypes.CORRECT_GUESS,
+  });
+  expect(newState).toBe(true);
+});
